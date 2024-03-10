a0d78ae139e6a43522a3fcbf76ca3f461e53cab0
Holly <103157554+Aki047@users.noreply.github.com>
2024-03-09 21:19:06 -0800

    Conditional Routing in an Express NodeJS Server 1

---
+++ Week 3/Conditional Routing in an Express NodeJS Server 1/Server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/foo', (req, res, next) => {
+    if (Math.random() < 0.5) {
+        res.send('sometimes this');
+    } else {
+        next(); // Pass control to the next route handler
+    }
+});
+
+app.get('/foo', (req, res) => {
+    res.send('and sometimes that');
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 - Not Found');
+});
+
